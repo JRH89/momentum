5da1bf0ccb5406da8fe548e20c599f278edf06d9
JRH89 <thegemlins@gmail.com>
2025-01-23 23:33:36 -0800

    k

--- src/app/Contact/page.jsx
+++ src/app/Contact/page.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ContactPage from "../../components/Contact";
 
+export const metadata = {
+  title: "Contact | Momentum Project Management",
+  description: "Reach out with any questions or concerns.",
+  url: "/Contact",
+};
+
 const Page = () => {
   return (
     <div>
