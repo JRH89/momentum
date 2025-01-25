3158d18630ea97a7f9616f03bf00821cf1c40bef
JRH89 <thegemlins@gmail.com>
2025-01-24 21:22:51 -0800

    k

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,14 +4,32 @@ module.exports = {
 	siteUrl: "https://momentum.hookerhillstudios.com",
 	generateRobotsTxt: true, // (optional) Generates a robots.txt file
 	exclude: [
-		'/Dashboard',
-		'/Customer',
-		'/Dashboard/*', // Exclude all sub-routes under /Dashboard
-		'/Customer/*',  // Exclude all sub-routes under /Customer
-		'/stripe/callback',
-		'/unsubscribe',
-		'/Admin',
-		'/Admin/*',
+		"/Dashboard",
+		"/Customer",
+		"/Dashboard/*", // Exclude all sub-routes under /Dashboard
+		"/Customer/*",  // Exclude all sub-routes under /Customer
+		"/stripe/callback",
+		"/unsubscribe",
+		"/Admin",
+		"/Admin/*",
 	],
-	sitemapXmlPath: 'public/sitemap.xml', // Custom sitemap path
+	sitemapXmlPath: "public/sitemap.xml", // Custom sitemap path
+	additionalPaths: async (config) => {
+		const dynamicRoutes = [
+			"/About/welcomeToMomentum",
+			"/About/accountCreation",
+			"/About/connectingStripe",
+			"/About/userDashboard",
+			"/About/customerDashboard",
+			"/About/invoicing",
+			"/About/projectPages",
+		];
+
+		return dynamicRoutes.map((route) => ({
+			loc: route,
+			changefreq: "daily",
+			priority: 0.7,
+			lastmod: new Date().toISOString(),
+		}));
+	},
 };
--- src/app/About/[activeSection]/page.jsx
+++ src/app/About/[activeSection]/page.jsx
@@ -1,10 +1,31 @@
 import siteMetadata from "../../../../siteMetadata";
 import ActiveAbout from "../../../components/page/ActiveAbout";
 
-export const metadata = {
-  title: `About | ${siteMetadata.title}`,
-  description: `Learn more about ${siteMetadata.title}`,
-};
+export async function generateMetadata({ params }) {
+  const { activeSection } = params;
+
+  const formattedTitle = activeSection
+    ? activeSection.replace(/([a-z])([A-Z])/g, "$1 $2")
+    : "About";
+
+  return {
+    title: formattedTitle
+      ? `${formattedTitle
+          .replace(/-/g, " ")
+          .replace(/\b\w/g, (char) => char.toUpperCase())} | ${
+          siteMetadata.title
+        }`
+      : `About | ${siteMetadata.title}`,
+    description: formattedTitle
+      ? `Learn more about ${formattedTitle
+          .replace(/-/g, " ")
+          .toLowerCase()} in ${siteMetadata.title}.`
+      : `Learn more about ${siteMetadata.title}.`,
+    url: formattedTitle
+      ? `${siteMetadata.siteUrl}/About/${formattedTitle}`
+      : `${siteMetadata.siteUrl}/About`,
+  };
+}
 
 export default function Page() {
   return (
